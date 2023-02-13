00df2a42aaeb971ae65b424eb4e40e48df016346
starfrost <mario64crashed@gmail.com>
2023-02-13 13:50:25 +0000

    Add project files.

---
+++ Core.h
@@ -0,0 +1,44 @@
+#pragma once
+
+// core defines go here
+
+// Version information
+#define E6_VERSION "0.0.1"
+#define E6_BUILD_DATE __DATE__ + __TIME__
+#define E6_BUILD_STRING "E6 Version " + E6_VERSION + " " + E6_BUILD_DATE
+
+// MSVC typedefs
+#if _MSC_VER
+typedef signed __int8 e6_int8;
+typedef unsigned __int8 e6_uint8;
+typedef signed __int16 e6_int16;
+typedef unsigned __int16 e6_uint16;
+typedef signed __int32 e6_int32;
+typedef unsigned __int32 e6_uint32;
+typedef signed __int32 e6_int64;
+typedef unsigned __int32 e6_uint64;
+
+#define E6_API __declspec(dllexport)
+// GCC typedefs
+#elif __GNUC__
+typedef __INT8_TYPE__ e6_int8;
+typedef __UINT8_TYPE__ e6_uint8;
+typedef __INT16_TYPE__ e6_int16;
+typedef __UINT16_TYPE__ e6_uint16;
+typedef __INT32_TYPE__ e6_int32;
+typedef __UINT32_TYPE__ e6_uint32;
+typedef __INT32_TYPE__ e6_int64;
+typedef __UINT32_TYPE__ e6_uint64;
+
+// we don't use GCC for windows so just define linux here
+
+#define E6_API __attribute__((visibility("default")))
+#endif
+
+// OS configuration
+
+#if _WIN32
+#define E6_BUILD_WINDOWS
+#define E6_BUILD_SDL
+#endif
+
